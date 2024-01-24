4c93ff799510af5357b586b3f10efc2a59062e54
Arun Ranjan Sahu <61779161+arunsahu159@users.noreply.github.com>
2024-01-24 10:35:39 +0530

    Update tensorboard.py
    commented summary Writer

--- ultralytics/utils/callbacks/tensorboard.py
+++ ultralytics/utils/callbacks/tensorboard.py
@@ -1,26 +1,26 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
-import contextlib
+# import contextlib
 
-from ultralytics.utils import LOGGER, SETTINGS, TESTS_RUNNING, colorstr
+# from ultralytics.utils import LOGGER, SETTINGS, TESTS_RUNNING, colorstr
 
-try:
-    # WARNING: do not move SummaryWriter import due to protobuf bug https://github.com/ultralytics/ultralytics/pull/4674
-    from torch.utils.tensorboard import SummaryWriter
+# try:
+#     # WARNING: do not move SummaryWriter import due to protobuf bug https://github.com/ultralytics/ultralytics/pull/4674
+#     from torch.utils.tensorboard import SummaryWriter
 
-    assert not TESTS_RUNNING  # do not log pytest
-    assert SETTINGS["tensorboard"] is True  # verify integration is enabled
-    WRITER = None  # TensorBoard SummaryWriter instance
-    PREFIX = colorstr("TensorBoard: ")
+#     assert not TESTS_RUNNING  # do not log pytest
+#     assert SETTINGS["tensorboard"] is True  # verify integration is enabled
+#     WRITER = None  # TensorBoard SummaryWriter instance
+#     PREFIX = colorstr("TensorBoard: ")
 
-    # Imports below only required if TensorBoard enabled
-    import warnings
-    from copy import deepcopy
-    from ultralytics.utils.torch_utils import de_parallel, torch
+#     # Imports below only required if TensorBoard enabled
+#     import warnings
+#     from copy import deepcopy
+#     from ultralytics.utils.torch_utils import de_parallel, torch
 
-except (ImportError, AssertionError, TypeError, AttributeError):
-    # TypeError for handling 'Descriptors cannot not be created directly.' protobuf errors in Windows
-    # AttributeError: module 'tensorflow' has no attribute 'io' if 'tensorflow' not installed
-    SummaryWriter = None
+# except (ImportError, AssertionError, TypeError, AttributeError):
+#     # TypeError for handling 'Descriptors cannot not be created directly.' protobuf errors in Windows
+#     # AttributeError: module 'tensorflow' has no attribute 'io' if 'tensorflow' not installed
+#     SummaryWriter = None
 
 
 def _log_scalars(scalars, step=0):
